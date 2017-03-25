867800aa09ccca08211401efa502fa3261f9c879
taoffke <taoffke@gmail.com>
2017-03-26 03:30:45 +0800

    user sign in, up, out, done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
-  end
+  	@user = User.new
+	end
+
+	def create
+  	@user = User.new(user_params)
+	  if @user.save
+	  	session[:user_id] = @user.id
+	  	flash[:success] = "Sign up successful!"
+	    redirect_to root_url
+	  else
+	    render "new"
+	  end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:username,
+																 :email,
+																 :password)
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+	def current_user
+		User.find(session[:user_id]) if session[:user_id] 
+  end
+
+  def page_title
+    if current_user != nil
+      "ToDo Diary | " + current_user.username
+    else
+      "ToDo Diary"
+    end
+  end
+
+  def signed_in?
+    true if current_user != nil
+  end
+
+  def task_status(task)
+    if task.status == false
+      return "Unsolve"
+    else
+      return "Solved!"
+    end
+  end 
+  
+  def mark_as_done(task)
+    task = Task.find(params[:id])
+    task.status = true
+    task.save
+  end
+
 end
